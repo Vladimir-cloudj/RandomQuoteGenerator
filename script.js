b99889eed32d485496e145df7bade8ae4a783f08
Vladimir <wizart88@gmail.com>
2022-02-11 23:04:42 +0400

    feat: add function randomBackgroundColor

--- script.js
+++ script.js
@@ -4,6 +4,30 @@ let	btn = document.getElementById('btn');
 
 const url = 'https://api.quotable.io/random';
 
+// const colors = ['#ff5733', '#f9ff33', '#3371ff'];
+const color = document.getElementById('color');
+const hex = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'];
+
+function getRandomNumber() {
+	// return Math.floor(Math.random() * colors.length);
+	return Math.floor(Math.random() * hex.length);
+}
+
+function generateHex() {
+	let hexColor = '#'
+	for (let i = 0; i < 6; i++) {
+		hexColor += hex[getRandomNumber()]
+	}
+	return hexColor;
+}
+
+function madeColors() {
+// let hexColor = colors[getRandomNumber()];
+	let hexColor = generateHex();
+	document.body.style.backgroundColor = hexColor; 
+	color.textContent = hexColor;
+}
+
 let getQuote = () => {
 	fetch(url)
 		.then((data) => data.json())
@@ -17,3 +41,4 @@ let getQuote = () => {
 };
 window.addEventListener('load', getQuote);
 btn.addEventListener('click', getQuote);
+btn.addEventListener('click', madeColors);
